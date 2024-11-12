9c3435e4b86a0e05c7f3ca6be38078b5d4374c04
stependiant <rodionmasalov34@gmail.com>
2024-11-12 22:28:17 +0300

    initial commit

---
+++ src/main/java/step/gamebot/GameBotApplication.java
@@ -0,0 +1,13 @@
+package step.gamebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/step/gamebot/GameBotApplicationTests.java
@@ -0,0 +1,13 @@
+package step.gamebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
